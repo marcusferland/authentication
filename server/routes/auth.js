ed9e95dc7e9de038f5c8fa6eeba36ad1ca8d6d3f
Marc Ferland <mferland@esentire.com>
2017-04-05 14:19:24 -0400

    Changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -119,7 +119,7 @@ router.post('/signup', (req, res, next) => {
 router.post('/login', (req, res, next) => {
   const validationResult = validateLoginForm(req.body);
   if (!validationResult.success) {
-    return res.status(400).json({
+    return res.status(200).json({
       success: false,
       message: validationResult.message,
       errors: validationResult.errors
@@ -129,13 +129,13 @@ router.post('/login', (req, res, next) => {
   return passport.authenticate('local-login', (err, token) => {
     if (err) {
       if (err.name === 'IncorrectCredentialsError') {
-        return res.status(400).json({
+        return res.status(200).json({
           success: false,
           message: err.message
         });
       }
 
-      return res.status(400).json({
+      return res.status(200).json({
         success: false,
         message: 'Could not process the form.',
         err: err
