3b4609c5e2792b1b44df05f436d9b2c1dff57f1a
Marc Ferland <mferland@esentire.com>
2017-04-09 19:37:19 -0400

    Added node.js cluster

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
+const cluster = require('cluster')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const numCPUs = require('os').cpus().length
 const config = require('./config')
 
-// connect to the database and load models
-require('./server/models').connect(config.dbUri)
+if (cluster.isMaster) {
 
-const app = express()
+  console.log(`Master cluster setting up ${numCPUs} workers...`)
 
-app.use(cors())
+  for (var i = 0; i < numCPUs; i++) cluster.fork()
 
-// tell the app to parse HTTP body messages
-app.use(bodyParser.urlencoded({ extended: false }))
+  cluster.on('online', worker => {
+    console.log(`Worker ${worker.process.pid} is online`)
+  })
 
-// pass the passport middleware
-app.use(passport.initialize())
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`)
+    console.log('Starting a new worker')
+    cluster.fork()
+  })
+}
+else {
 
-// load passport strategies
-const localSignupStrategy = require('./server/passport/local-signup')
-const localLoginStrategy = require('./server/passport/local-login')
-passport.use('local-signup', localSignupStrategy)
-passport.use('local-login', localLoginStrategy)
+  const app = express()
 
-// pass the authorization checker middleware
-const authCheckMiddleware = require('./server/middleware/auth-check')
-app.use('/api', authCheckMiddleware)
+  // connect to the database and load models
+  require('./server/models').connect(config.dbUri)
 
-// routes
-const authRoutes = require('./server/routes/auth')
-const apiRoutes = require('./server/routes/api')
-app.use('/auth', authRoutes)
-app.use('/api', apiRoutes)
+  app.use(cors())
 
-// start the server
-app.listen(config.serverPort, () => {
-  console.log(`Server is running on http://localhost:${config.serverPort} or http://127.0.0.1:${config.serverPort}`)
-})
+  // tell the app to parse HTTP body messages
+  app.use(bodyParser.urlencoded({ extended: false }))
+
+  // pass the passport middleware
+  app.use(passport.initialize())
+
+  // load passport strategies
+  const localSignupStrategy = require('./server/passport/local-signup')
+  const localLoginStrategy = require('./server/passport/local-login')
+  passport.use('local-signup', localSignupStrategy)
+  passport.use('local-login', localLoginStrategy)
+
+  // pass the authorization checker middleware
+  const authCheckMiddleware = require('./server/middleware/auth-check')
+  app.use('/api', authCheckMiddleware)
+
+  // routes
+  const authRoutes = require('./server/routes/auth')
+  const apiRoutes = require('./server/routes/api')
+  app.use('/auth', authRoutes)
+  app.use('/api', apiRoutes)
+
+  // start the server
+  app.listen(config.serverPort, () => {
+    console.log(`Server is running on http://localhost:${config.serverPort} or http://127.0.0.1:${config.serverPort}`)
+  })
+}
